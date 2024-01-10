90ce50df1ef661dfd527131ef50927bec2cc0396
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-10 16:41:16 +0100

    retrieve elements from the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log('JS OK');
+
+// Recupero gli elementi dal DOM - Retrieve elements from the DOM
+const button = document.querySelector('button');
+const resultElement = document.querySelector('#section-grid .grid');
+console.log(button, resultElement);
