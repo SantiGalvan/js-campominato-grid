dcafa847bc213dac562d28a0081f9ea124779a5d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-10 16:58:43 +0100

    added function createCells

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 console.log('JS OK');
 
-// Recupero gli elementi dal DOM - Retrieve elements from the DOM
+// - 1 Recupero gli elementi dal DOM - Retrieve elements from the DOM
 const button = document.querySelector('button');
 const resultElement = document.querySelector('#section-grid .grid');
 console.log(button, resultElement);
+
+// - 2 Creo le variabili per le righe e le colonne
+const rows = 10;
+const cells = 10;
+const totalCells = rows * cells;
+console.log(totalCells);
+
+// ? FUNZIONI 
+// - 3 Creo la funzione per la cella
+const createCells = (number) => {
+    const newCell = document.createElement('div');
+    newCell.classList.add('cell');
+    newCell.innerText = number;
+    return newCell;
+}
