c6a16ce00f73635507264c9cd1815a9710b582ad
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-11 15:44:22 +0100

    fixed name tag header

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK');
 // - 1 Recupero gli elementi dal DOM - Retrieve elements from the DOM
 const button = document.querySelector('button');
 const resultElement = document.querySelector('#section-grid .grid');
-const formElement = document.querySelector('.right-head form');
+const formElement = document.querySelector('.right-header form');
 const selectElement = document.getElementById('user-select');
 console.log(formElement, selectElement);
 
